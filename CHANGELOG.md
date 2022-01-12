942cccc2b25928c9fa8465060872261b36eba9e2
Stephen Suffian <stephen.suffian@pm.me>
2022-01-11 20:49:01 -0500

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,11 @@ Development
 
 * Placeholder
 
+0.2.6
+-----
+
+* Placeholder
+
 0.2.5
 -----
 
--- phillydb/__version__.py
+++ phillydb/__version__.py
@@ -1,2 +1,2 @@
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 __description__ = "A python client for accessing open property data for Philadelphia"
